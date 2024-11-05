e6b7f21a479371ea57da574b51d7342a706e6edf
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-11-05 23:35:37 +0530

    test case 1 - user is able to signup

---
+++ tests/index.test.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// test("adds 1 + 2 to equal 3", () => {
+//   let ans = sum(1, 2);
+//   expect(ans).toBe(3);
+// });
+
+//describe Blocks : In Jest, describe blocks are used to organize and group related tests into logical sections. This can be especially helpful for larger test files where grouping tests by function or feature improves readability and structure. Each describe block can contain individual tests (using test or it functions) or nested describe blocks for further sub-grouping.
+
+describe("Authentication", () => {
+  test("User is able to signUp", () => {
+    const username = "jatin" + Math;
+  });
+});
