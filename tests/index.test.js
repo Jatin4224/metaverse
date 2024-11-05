7c8264b161dbf54d14aa49ff8fa5c3ff27eb3fab
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-11-06 00:14:45 +0530

    test2-Signup Request failes if the username is empty

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,8 +9,42 @@ function sum(a, b) {
 
 //describe Blocks : In Jest, describe blocks are used to organize and group related tests into logical sections. This can be especially helpful for larger test files where grouping tests by function or feature improves readability and structure. Each describe block can contain individual tests (using test or it functions) or nested describe blocks for further sub-grouping.
 
+//hardcoded backend url - we are doing test driven dev in which we will write test first nd backend later.
+const BACKEND_URL = "http://localhost:3000";
+
 describe("Authentication", () => {
-  test("User is able to signUp", () => {
-    const username = "jatin" + Math;
+  test("User is able to signUp only once", async () => {
+    const username = "jatin" + Math.random(); //jatin31327
+    const password = 12345678;
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+
+    //In HTTP, a status code 200 means "OK"
+    expect(response.statusCode).toBe(200);
+
+    //The HTTP status code 400 means "Bad Request"
+    const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+
+    //In HTTP, a status code 200 means "OK"
+    expect(updatedResponse.statusCode).toBe(400);
+  });
+
+  test("Signup Request failes if the username is empty", async () => {
+    const username = `Jatin-${Math.random()}`;
+    const password = "123456";
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      password,
+    });
+
+    expect(response.statusCode).toBe(400);
   });
 });
