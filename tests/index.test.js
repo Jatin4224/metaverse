8d1e0f9a5ca2e0746914ff8cf856444cd0be210a
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-11-12 03:19:26 +0530

    describe block -Admin Endpoints test1-User is not able to hit admin Endpoints

--- tests/index.test.js
+++ tests/index.test.js
@@ -190,11 +190,19 @@ describe("User avatar information", () => {
 
     //getting avatar id
 
-    const avatarResponse = await axios.post(`${BACKEND_URL}/api/v1/avatar`, {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
-      name: "Timmy",
-    });
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+        name: "Timmy",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
     avatarId = avatarResponse.data.avatarId;
   });
@@ -748,12 +756,11 @@ describe("Arena endpoints", () => {
   });
 });
 
-describe("Create an element", () => {
+describe("Admin Endpoints", () => {
   let adminToken;
   let adminId;
   let userToken;
   let userId;
-  b;
 
   beforeAll(async () => {
     const username = `jatin-${Math.random()}`;
@@ -804,5 +811,73 @@ describe("Create an element", () => {
     userToken = userSigninResponse.data.token;
   });
 
-  test("User is not able to hit admin Endpoints", () => {});
+  test("User is not able to hit admin Endpoints", async () => {
+    //creating element
+    const elementResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/element`,
+
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+        width: 1,
+        height: 1,
+        static: true,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${adminToken}`,
+        },
+      }
+    );
+    ///creating Maps
+    const mapResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/map`,
+      {
+        thumbnail: "https://thumbnail.com/a.png",
+        dimensions: "100x200",
+        defaultElements: [],
+      },
+      {
+        headers: {
+          authorization: `Bearer ${userToken}`,
+        },
+      }
+    );
+
+    //create an Avatar
+
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+        name: "Timmy",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${userToken}`,
+        },
+      }
+    );
+
+    //update an element
+
+    const updateElementResponse = await axios.put(
+      `${BACKEND_URL}/api/v1/admin/element/123`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${userToken}`,
+        },
+      }
+    );
+
+    expect(elementResponse.statusCode).toBe(403);
+    expect(mapResponse.statusCode).toBe(403);
+    expect(avatarResponse.statusCode).toBe(403);
+    expect(updateElementResponse.statusCode).toBe(403);
+  });
 });
